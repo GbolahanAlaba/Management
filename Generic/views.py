850046888b635f992a88da13aede950309197bc7
GbolahanAlaba <thegbolahanalaba@gmail.com>
2023-06-08 02:06:08 +0100

    Latest Commit

--- Generic/views.py
+++ Generic/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.db.models import Sum
@@ -12,6 +12,25 @@ def homepage(request):
     return render(request, "index.html")
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        x = 'gbolahan'
+        y = 'pass'
+
+        if username != x and password != y:
+            messages.info(request, 'Invalid Login')
+            return render(request, 'signin.html')
+        elif username != x:
+            messages.info(request, 'Incorrect Username')
+            return render(request, 'signin.html')
+        elif password != y:
+            messages.info(request, 'Incorrect Password')
+            return render(request, 'signin.html')
+        else:
+            return redirect('homepage')
+
     return render(request, "signin.html")
 
 # def createaccount(request):
